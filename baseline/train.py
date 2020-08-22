c99e12cce298077e5ee0e0f99e7c7cd5a90a3ab7
Junseop <gaon0403@gmail.com>
2020-08-22 19:58:05 +0900

    Fix: Evaluation during train took too long time

--- baseline/train.py
+++ baseline/train.py
@@ -62,7 +62,7 @@ def main(args):
     learning_rate = 1e-3
     num_epochs = 3
     log_step = args.log_step
-    save_step = 500
+    save_step = 1500
     checkpoint_dir = args.checkpoint_dir
 
     encoder = CNN(embed_size)
@@ -96,7 +96,6 @@ def main(args):
         for epoch in tqdm(range(initial_epoch, num_epochs)):
 
             for step, (images, captions, lengths) in enumerate(tqdm(train_loader), start=initial_step):
-
                 # Set mini-batch dataset
                 images = utils.to_var(images, volatile=True)
                 captions = utils.to_var(captions)
@@ -118,51 +117,67 @@ def main(args):
                     features = encoder(images)
                     outputs = decoder(features, captions, lengths)
 
-                print(features)
+                # print(features)
 
                 train_loss = criterion(outputs, targets)
+                # print('criterion')
                 losses_train.append(train_loss.item())
+                # print('append')
                 train_loss.backward()
+                # print('backward')
                 optimizer.step()
+                # print('step')
 
                 # Run validation set and predict
                 if step % log_step == 0:
                     encoder.batchnorm.eval()
-                    # run validation set
-                    batch_loss_val = []
-                    for val_step, (images, captions, lengths) in enumerate(val_loader):
-                        images = utils.to_var(images, volatile=True)
-                        captions = utils.to_var(captions, volatile=True)
+                    if args.val_while_train:
+                        # run validation set
+                        with torch.no_grad():
+                            batch_loss_val = []
+                            for val_step, (images, captions, lengths) in enumerate(tqdm(val_loader)):
+                                images = utils.to_var(images, volatile=True)
+                                captions = utils.to_var(
+                                    captions, volatile=True)
 
-                        targets = pack_padded_sequence(
-                            captions, lengths, batch_first=True)[0]
-                        features = encoder(images)
-                        outputs = decoder(features, captions, lengths)
-                        val_loss = criterion(outputs, targets)
-                        batch_loss_val.append(val_loss.item())
+                                targets = pack_padded_sequence(
+                                    captions, lengths, batch_first=True)[0]
+                                features = encoder(images)
+                                outputs = decoder(features, captions, lengths)
+                                val_loss = criterion(outputs, targets)
+                                batch_loss_val.append(val_loss.item())
 
-                    losses_val.append(np.mean(batch_loss_val))
+                            losses_val.append(np.mean(batch_loss_val))
 
-                    # predict
-                    sampled_ids = decoder.sample(features)
-                    sampled_ids = sampled_ids.cpu().data.numpy()[0]
-                    sentence = utils.convert_back_to_text(sampled_ids, vocab)
-                    print('Sample:', sentence)
+                            # predict
+                            sampled_ids = decoder.sample(features)
+                            sampled_ids = sampled_ids.cpu().data.numpy()[0]
+                            sentence = utils.convert_back_to_text(
+                                sampled_ids, vocab)
+                            print('Sample:', sentence)
 
-                    true_ids = captions.cpu().data.numpy()[0]
-                    sentence = utils.convert_back_to_text(true_ids, vocab)
-                    print('Target:', sentence)
+                            true_ids = captions.cpu().data.numpy()[0]
+                            sentence = utils.convert_back_to_text(
+                                true_ids, vocab)
+                            print('Target:', sentence)
 
-                    print('Epoch: {} - Step: {} - Train Loss: {} - Eval Loss: {}'.format(
-                        epoch, step, losses_train[-1], losses_val[-1]))
+                            print('Epoch: {} - Step: {} - Train Loss: {} - Eval Loss: {}'.format(
+                                epoch, step, losses_train[-1], losses_val[-1]))
+                    else:
+                        print('Epoch: {} - Step: {} - Train Loss: {}'.format(
+                            epoch, step, losses_train[-1]))
                     encoder.batchnorm.train()
+
+                # print('endif 0')
 
                 # Save the models
                 if (step + 1) % save_step == 0:
+                    print('Saving model....')
                     utils.save_models(encoder, decoder, optimizer, step,
                                       epoch, losses_train, losses_val, checkpoint_dir)
                     utils.dump_losses(losses_train, losses_val, os.path.join(
                         checkpoint_dir, 'losses.pkl'))
+                # print('endif 1')
 
     except KeyboardInterrupt:
         pass
@@ -188,10 +203,11 @@ if __name__ == '__main__':
     parser.add_argument('--sample', default=False,
                         action='store_true', help='just show result, requires --checkpoint_file')
     parser.add_argument('--log_step', type=int,
-                        default=125, help='number of steps in between calculating loss')
+                        default=4000, help='number of steps in between calculating loss')
     parser.add_argument('--num_hidden', type=int,
                         default=512, help='number of hidden units in the RNN')
     parser.add_argument('--embed_size', type=int,
                         default=512, help='number of embeddings in the RNN')
+    parser.add_argument('--val_while_train', action='store_true')
     args = parser.parse_args()
     main(args)
